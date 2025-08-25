b32c0e2cc51231e2302a63b4cddf39840e103904
donniexoxo <dmcgriff192@boyslatin.net>
2025-08-25 13:14:36 +0000

    class work

---
+++ unit 1/datatypes.py
@@ -0,0 +1,36 @@
+#strings- anything wrraped in quotes
+# quotes sting the data together
+"Donnell mcgriff"
+'good moring, have a good day'
+'123'
+#intergers- whole numbers.
+12
+13
+#comments- invisble text that only
+# humans can read and see
+#comments are created with the hashtag symbol(#)
+# intergers - whole numbers
+10
+120
+34000
+# floats- decimal numbers
+0.1
+20.03
+30.234
+10.0
+10.0000001232
+#boolean-values that represent true and false
+true
+false
+
+0
+1
+
+''
+'abc'
+
+# variable = a container for data
+# to create variable, we first give it a name
+#then we assighn it a value (data)
+
+schoolname='boys latin'
