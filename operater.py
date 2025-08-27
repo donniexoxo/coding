70a88fc3a43cd2345ff1e0848b55b29a5bb90304
donniexoxo <dmcgriff192@boyslatin.net>
2025-08-27 13:37:21 +0000

    class work

---
+++ operater.py
@@ -0,0 +1,25 @@
+# operator
+# operators are a construct (built-in system)that give
+#data types more actions and power
+
+# arithmetic operator family
+#arithmetic operators are simply math operators.
+
+print(2+3)
+print(20-100)
+print(40/2)
+print(30.9*10)
+
+#assiment operator family
+# these operator assign values to variables
+# (otherwise know as containers)
+#key-value pairs
+
+# variable=container for data
+
+schoolname= "Boys latin high"
+age=16
+
+age += 1.5
+
+print(age) 
