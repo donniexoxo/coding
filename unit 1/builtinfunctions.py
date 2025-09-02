801abd68bf98b4c0d14ba148334bdffbc68ca1d2
donniexoxo <dmcgriff192@boyslatin.net>
2025-09-02 13:37:15 +0000

    class work

---
+++ unit 1/builtinfunctions.py
@@ -0,0 +1,13 @@
+#built-functuions-prewritten code that is stored
+#in the programming language. so long as you know the
+# keyword you can use its power
+
+# floats()function- allow us to transform a whole number
+#into a decimal number
+
+transfer=50
+
+bank=12.00
+
+print(float(transfer)+bank)
+
