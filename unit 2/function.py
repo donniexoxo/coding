7dd08141a2481aa67152710ac99a14c485879735
donniexoxo <dmcgriff192@boyslatin.net>
2025-09-11 13:37:45 +0000

    class work

--- unit 2/function.py
+++ unit 2/function.py
@@ -1,27 +1,68 @@
 # function - a function is a set of codde instructions
+# a custom name that the computer will run.
+
+# Function syntax (Rules of how its written)
+# Function have 2 phases: function definition and
+# function call
+
 # for a computer to follow
 
 # a function comes in 2 phases
 
 # phasesz 1: function defintion
+# we are describing the instruction for our custom mode.
+# we are adding logic to the computers vocabulary.
+# this code does not run- it only give the computer the meaning
+# not the action
+
+
 
 # this code to do. it only runs when we call it
 def example():
-      print('good moring')
+      print('good moring') # 1 instruction print good moring
 
 def example2(I):
       print('good day')
 
 # phase 2: function call/invocation
+# once we have the definion, we can now run the instructions
+# by writing the function name.
+
 # when we are ready to use a function,
 # we write ito use it.
 example()
-example2()
 
+# we indent of inform the computer that are about to write
+# code instructions. if we dont, we will get an error
+
+
+def example2():
+     print('good moring')
+     a= input('enter a number')
+     print(a)
 
 
 def math():
-      a = input('enter a number')
-      b = 10
-      print(a + b)
+      a = input('please enter a number')
+      b = 30
+      print("here is your result")
+      print(int(a) + b)
+
 math()
+
+
+
+# create a function that calculates 2 unique user inputted numbers
+
+def calculate():
+      a = input(' please enter a number')
+      b = input('please enter another number:')
+      print(int(a) + int(b))
+      print(int(a) + int(b))
+      print(int(a) + int(b))
+      print(int(a) + int(b))
+      print(int(a) + int(b))
+      print(int(a) + int(b))
+      print(int(a) + int(b))
+
+calculate()
