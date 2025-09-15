1a9e27ba254b94f4396bafd327d9b16963b82b7e
donniexoxo <dmcgriff192@boyslatin.net>
2025-09-15 13:17:35 +0000

    class work

---
+++ functiondata.py
@@ -0,0 +1,18 @@
+#How to create a function with data
+
+#step 1. Function ddefinition
+def bnbRefund(accountNumber, refundamount): # paramters are placeholders
+   print('account number:  '  + str(accountnumber))
+   print("refund amount: $ " + str(refundamopunt))
+
+# step 2. function call (invocation)
+bnbrefund(19292929, 3000) # arguements are real data 
+
+name= "Donnell mcgriff"
+birthday="August 6, 2008"
+
+
+def name_bday(name,bday):
+       print('my name is + ' name + "my bday is " + bday)
+
+name_bday('')
