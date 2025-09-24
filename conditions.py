a4a3d5215023f71f11b4e8b5e87201a56b2d3d3b
donniexoxo <dmcgriff192@boyslatin.net>
2025-09-24 13:37:22 +0000

    class work

--- conditions.py
+++ conditions.py
@@ -19,6 +19,23 @@ elif weather== "chilly":
 else:
     print("Have a good day.")
 
+
+
+
+
+down = input("what down is it?")
+yards = input("how man yards do you need to get another first down?")
+
+if down ==3 and yards < 10:
+    print("bomb the ball- pass")
+elif down == 2 and yards < 8:
+    print('short pass')
+elif down == 3 and yards > 10:
+    print('run it')
+else:#this is the exit; assumes it is 4th dowm
+    print("punt")
+
+
 #create a conditional block of code that acts as a 
 #password checker. the user should be able to input a
 #password. if it is correct, they should get a message
@@ -31,3 +48,17 @@ if password== "abc123":
     print('welcome! you are logged in.')
 else:
     print('incorrsct password.')
+
+
+age=17
+
+if age == 17 and age > 17:
+    print("allowed to get permint")
+elif age == 14 and age > 14:
+    print('is not allowed to get there permint')
+
+
+
+def permitcheck(age):
+    if age >= 16:
+        print("congrats, you can begin learning to drive.")
