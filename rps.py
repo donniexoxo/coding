371cf09e92ae5eb3c3f81792862b935bd184869c
donniexoxo <dmcgriff192@boyslatin.net>
2025-11-20 14:36:54 +0000

    class work

--- rps.py
+++ rps.py
@@ -12,3 +12,27 @@
 "computer makes a random selection"
 "determine and the user/player if they won,lose,or tied "
 "and keep track of score and round"
+
+
+
+
+
+
+
+def rpsdgame():
+    print("welcome to rock paper scissor: the game!")
+    print("please select one of the following: ")
+    print ("enter, p to start game, ")
+    print ("or enter to rules")
+    selection = input()
+    if selection == 'r':
+        print("here are the game rules . . .")
+    elif selection == 'p':
+        print('the game is starting . . .')
+        choice = input("please make a selection, r = rock, p = papaer, s = scissor")
+        choicecui = random.choice(rpsoptions_cpu)
+        print("user selecten " + choice)
+    
+    else:
+        print('sorry, we didint understand your entry')
+        choice = input("")
