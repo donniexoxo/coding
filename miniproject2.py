df69a264f9a29ab80bfe4a5350b5aa66393e78a3
donniexoxo <dmcgriff192@boyslatin.net>
2025-12-12 14:37:11 +0000

    mini 2

---
+++ miniproject2.py
@@ -0,0 +1,49 @@
+#build gpa calculator 
+# take in numbers and average them out based on numbers given.
+
+# 2. take in subject area as string.
+
+
+
+
+
+#def gpacalculator():
+    #print("which subject is the gpa for")
+    #subject = input()
+    #print('the subject is ' + subject)
+    #gradelist = []
+    #week = 1
+    
+
+
+def gpacalculator():
+    grade = 0
+    print("which subject is the gpa for")
+    subject = input()
+    print('the subject is ' + subject)
+    print('please enter a grade for week 1')
+    week1grade= input()
+    print(week1grade)
+if gpacalculator():
+    grade = +1
+    print('what subject id this grade for')
+    subject = input()
+    print('the subject is' + subject)
+    print('please enter a grade for week 2')
+    week2grade= input()
+    print(week2grade)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gpacalculator()
