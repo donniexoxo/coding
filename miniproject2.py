40008658f9cc0c328417af8f59d1939f8f4f4107
donniexoxo <dmcgriff192@boyslatin.net>
2025-12-17 14:03:34 +0000

    12/16

--- miniproject2.py
+++ miniproject2.py
@@ -34,6 +34,27 @@ if gpacalculator():
     print(week2grade)
 
 
+def gpaCalculator():
+    print(' What subject is this GPA for? ')
+    subject = input()
+    print("the user entered: "+ subject)
+    week = 1
+    sum = 0
+    for x in range (18):
+        print('Please enter the grade for week : '+ str(week))
+        grade = int(input())
+        sum += grade
+        week += 1
+        gpa = sum / 10
+    if gpa > 70 and gpa < 80:
+        print('C')
+    elif gpa > 80  and gpa < 90:
+        print('B')
+    elif gpa > 90  and gpa <100:
+        print('A')
+    else:
+        print('F')
+    print('your gpa for '+ subject + ' is ' + str(gpa))
 
 
 
