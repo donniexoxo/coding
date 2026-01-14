b73e27c0fb3ebcf36c280aebe2fb6f5739e34ac2
donniexoxo <dmcgriff192@boyslatin.net>
2026-01-14 14:37:15 +0000

    class work 1/14

--- bankappcode.py
+++ bankappcode.py
@@ -16,4 +16,36 @@ def paycheckfilter(payrate, hours,  daysworked):
     print("retirementaccount account blance: " + str(retirementaccount))
     print("checking  account blance: " + str(checkingaccount))
 
-paycheckfilter(30.00, 6, 4)
+#paycheckfilter(30.00, 6, 4)
+
+
+
+
+
+def ridesharecalculator(miles, surgeprice, discount):
+    base_fare = 3.00
+    surge_fare =3.75
+    cost_per_mile = 2.00
+
+
+    total = base_fare + miles * cost_per_mile
+
+
+    if surgeprice == true:
+        print("the final price for this ride is $" + str(base_fare + surgerpricing (miles* cost_per_mile)))
+
+    else:
+        print("the final price for this ride is $" + str(base_fare + surgerpricing (miles* cost_per_mile)))
+
+    if disccount == true:
+         discountprice = total * total* .15
+         print("the final price for this ride is $" + str(base_fare + surgerpricing (miles* cost_per_mile)))
+    else:
+        print("the final price for this ride is $" + str(base_fare + surgerpricing (miles* cost_per_mile)))
+
+
+
+
+
+    
+ridesharecalculator(3, false,false)
